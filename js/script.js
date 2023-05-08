1000c1e52f07915acc34f2a7b202e5c46f418d53
GraydonE <124379361+GraydonE@users.noreply.github.com>
2023-05-08 14:35:38 +0000

    it works

--- js/script.js
+++ js/script.js
@@ -10,24 +10,29 @@
     
   // Declare age and day of the week and program feedback
   let age = parseInt(document.getElementById('age').value);
-  let day = document.getElementById ('day') ;  
-  let programFeedback = "" ;
+  let day = document.getElementById('day').value;  
+  let programFeedback = "";
     
   // Declare program feedback depending on age.
-  if (age < 5 || age > 95){
-    programFeedback = "You can get in for free!"
+  if ((age < 5) || (age > 95)) {
+    programFeedback = "You can get in for free!";
   }
 
-  else if (age >= 12 && age <= 21 || day == "tuesday" || day == "thursday") {
+  else if (((age >= 12) && (age <= 21)) || ((day == "tuesday") || (day == "thursday"))) {
     programFeedback = "You get a discounted price!";
   }
 
-  else {
-    programFeedback = "You pay the regular price"   
+  else if ((age <=5) || (age >21)){
+    programFeedback = "You pay the regular price"; 
   }
 
+  else {
+    programFeedback = "Please enter a valid age."
+  }
+  
+
     
-  // output the price for museume entry
+  // output the price for museum entry
     
   document.getElementById('program-feedback').innerHTML = programFeedback;
   }
